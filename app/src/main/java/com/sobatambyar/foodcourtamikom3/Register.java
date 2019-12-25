95a3aa48035d8ed9c0f65eeb0cedb2cf1328ebb8
irwansyah <irwansyah.ir@students.amikom.ac.id>
2019-12-25 23:09:09 +0700

    register
    java register sudah.. phpnya sudah. tinggal di cek. di leptop ak gak bisa COK!!

--- app/src/main/java/com/sobatambyar/foodcourtamikom3/Register.java
+++ app/src/main/java/com/sobatambyar/foodcourtamikom3/Register.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -17,6 +18,10 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -24,7 +29,7 @@ public class Register extends AppCompatActivity {
     private EditText name, email, password;
     private Button btn_regist;
     private ProgressBar loading;
-    private static String URL_REGIST = "";
+    private static String URL_REGIST = "http://192.168.1.104/php/register.php";
     TextView txtsignin;
 
     @Override
@@ -35,15 +40,18 @@ public class Register extends AppCompatActivity {
         name = findViewById(R.id.name);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
+        btn_regist = findViewById(R.id.btn_regist);
+        txtsignin = findViewById(R.id.txtsignin);
 
         btn_regist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //
+                Regist();
             }
         });
     }
-    private void Regist(){
+
+    private void Regist() {
         loading.setVisibility(View.VISIBLE);
         btn_regist.setVisibility(View.GONE);
 
@@ -55,34 +63,57 @@ public class Register extends AppCompatActivity {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
 
+                            if (success.equals("1")) {
+                                Toast.makeText(Register.this, "Register Success", Toast.LENGTH_SHORT).show();
+
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+
+                            Toast.makeText(Register.this, "Register Error!" + email.toString(), Toast.LENGTH_SHORT).show();
+
+                            loading.setVisibility(View.GONE);
+                            btn_regist.setVisibility(View.VISIBLE);
+                        }
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
 
+                        Toast.makeText(Register.this, "Register Error!" + error.toString(), Toast.LENGTH_SHORT).show();
+                        loading.setVisibility(View.GONE);
+                        btn_regist.setVisibility(View.VISIBLE);
                     }
-                })
-        {
+                }) {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
-                return super.getParams();
+                Map<String, String> params = new HashMap<>();
+                params.put("name", name);
+                params.put("email", email);
+                params.put("password", password);
+                return params;
             }
         };
 
-
-        txtsignin=(TextView)findViewById(R.id.txtsignin);
-        txtsignin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent2 = new Intent(Register.this,MainActivity.class);
-                startActivity(intent2);
-            }
-        });
-
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayShowHomeEnabled(true);
-        actionBar.setIcon(R.drawable.ic_syifalogo);
+//        txtsignin=(TextView)findViewById(R.id.txtsignin);
+//        txtsignin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent2 = new Intent(Register.this,MainActivity.class);
+//                startActivity(intent2);
+//            }
+//        });
+//
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.setDisplayShowHomeEnabled(true);
+//        actionBar.setIcon(R.drawable.ic_syifalogo);
+//    }
+//}
     }
 }
