5059f5555125a621a00b3f45896dc776a96c6194
irwansyah <irwansyah.ir@students.amikom.ac.id>
2019-12-25 14:08:10 +0700

    java register

--- app/src/main/java/com/sobatambyar/foodcourtamikom3/Register.java
+++ app/src/main/java/com/sobatambyar/foodcourtamikom3/Register.java
@@ -6,16 +6,71 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class Register extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 
+import java.util.Map;
+
+
+public class Register extends AppCompatActivity {
+    private EditText name, email, password;
+    private Button btn_regist;
+    private ProgressBar loading;
+    private static String URL_REGIST = "";
     TextView txtsignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        loading = findViewById(R.id.loading);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+        btn_regist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+            }
+        });
+    }
+    private void Regist(){
+        loading.setVisibility(View.VISIBLE);
+        btn_regist.setVisibility(View.GONE);
+
+        final String name = this.name.getText().toString().trim();
+        final String email = this.name.getText().toString().trim();
+        final String password = this.name.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_REGIST,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                return super.getParams();
+            }
+        };
+
 
         txtsignin=(TextView)findViewById(R.id.txtsignin);
         txtsignin.setOnClickListener(new View.OnClickListener() {
